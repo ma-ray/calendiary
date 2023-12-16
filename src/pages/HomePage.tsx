e0079215569872da3e1f4d30d72089182d804bd0
Raymond Ma <60418739+ma-ray@users.noreply.github.com>
2023-12-16 16:59:03 -0500

    set height

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,9 +2,13 @@ import Calendar from '../components/Calendar'
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <p>home page</p>
-      <Calendar month={11} year={2023} />
+    <div className="h-screen">
+      <div className="absolute left-0 top-0 p-6">
+        <h1 className="text-xl">home page</h1>
+      </div>
+      <div className="flex justify-center items-center h-full">
+        <Calendar month={11} year={2023} />
+      </div>
     </div>
   )
 }
