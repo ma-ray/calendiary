6a4f71017357ab0ec7426624c254b18452869b1c
Raymond Ma <60418739+ma-ray@users.noreply.github.com>
2023-12-17 20:13:37 -0500

    add scroll snap to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
+import { useRef } from 'react'
 import Calendar from '../components/Calendar'
 import { motion, useScroll, useSpring } from 'framer-motion'
 
 const HomePage = () => {
-  const { scrollYProgress } = useScroll()
+  const calendarListRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    container: calendarListRef,
+  })
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
@@ -14,11 +18,19 @@ const HomePage = () => {
       <div className="fixed left-0 top-0 p-6">
         <h1 className="text-xl">home page</h1>
       </div>
-      {Array.from({ length: 12 }, (_, i) => i).map((i) => (
-        <div key={i} className="flex justify-center items-center h-full">
-          <Calendar month={i} year={2023} />
-        </div>
-      ))}
+      <div
+        ref={calendarListRef}
+        className=" h-screen snap-mandatory snap-y overflow-y-scroll"
+      >
+        {Array.from({ length: 12 }, (_, i) => i).map((i) => (
+          <div
+            key={i}
+            className="snap-center relative flex justify-center items-center h-full"
+          >
+            <Calendar month={i} year={2023} />
+          </div>
+        ))}
+      </div>
       <motion.div
         style={{ scaleX }}
         className="fixed bottom-0 left-0 origin-left h-2 w-full bg-slate-900"
