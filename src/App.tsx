b12ee8a010575ad2068ea3cd5e6a1b438c166f9d
Raymond Ma <60418739+ma-ray@users.noreply.github.com>
2024-01-07 21:19:37 -0500

    remove not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage.tsx'
 import DiaryPage from './pages/DiaryPage.tsx'
 import { SettingsProvider } from './context/SettingsContext.tsx'
-import NotFoundPage from './pages/NotFoundPage.tsx'
 
 const router = createBrowserRouter([
   {
     path: '*',
-    element: <NotFoundPage />,
+    element: <Navigate to="/" replace />,
   },
   {
     path: '/',
